cc5302a7a61dedba8530c15691a772be68a77fc4
Pedro <pedrohenrique_3333@outlook.com>
2023-10-08 18:55:49 -0300

    1.0

--- TodoList/assets/js/main.js
+++ TodoList/assets/js/main.js
@@ -8,7 +8,11 @@ btnTarefa.addEventListener("click", function (e) {
   criaTarefa(inputTarefa.value);
 });
 
-inputTarefa.addEventListener("keypress");
+inputTarefa.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    criaTarefa(inputTarefa.value);
+  }
+});
 
 function criaLi() {
   // apenas cria o li
@@ -16,9 +20,59 @@ function criaLi() {
   return li;
 }
 
+function limpaInput() {
+  inputTarefa.value = "";
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+  li.innerText += "";
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerText = "Apagar";
+  botaoApagar.setAttribute("class", "apagar");
+  li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
   //coloca a li em texto
   const li = criaLi();
   li.innerText = textoInput;
   tarefas.appendChild(li); //coloca essa li dentro da ul para aparecer na pagina
+  limpaInput();
+  criaBotaoApagar(li);
+  salvarTarefas();
 }
+
+document.addEventListener("click", function (e) {
+  const el = e.target;
+
+  if (el.classList.contains("apagar")) {
+    el.parentElement.remove(); //deleta a tarefa
+    salvarTarefas();
+  }
+});
+
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll("li");
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("Apagar", "").trim(); // trim remove o espaco em cada elemento da array
+    listaDeTarefas.push(tarefaTexto);
+  }
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem("tarefas", tarefasJSON);
+  console.log(tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+  const tarefas = localStorage.getItem("tarefas");
+  const listaDeTarefas = JSON.parse(tarefas);
+  console.log(listaDeTarefas);
+
+  for (let tarefa of listaDeTarefas) {
+    criaTarefa(tarefa);
+  }
+}
+adicionaTarefasSalvas();
